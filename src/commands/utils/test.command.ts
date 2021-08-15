69f820db636811e1c3d16aebe9190626a946293a
null <dwsong07@naver.com>
2021-08-15 17:30:07 +0900

    Remove test command

--- src/commands/utils/test.command.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { Command } from "../../types";
-
-export default {
-    data: new SlashCommandBuilder().setName("test").setDescription("test"),
-    async execute(interaction) {
-        interaction.reply("ㅗ");
-    },
-} as Command;
